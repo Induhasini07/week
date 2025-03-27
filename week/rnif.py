06a380091c79650ba7bc7eb19fa66797d3f78f2a
Induhasini07 <hasinimittu6@gmail.com>
2025-03-27 10:28:54 +0530

    Create rnif.py

---
+++ week/rnif.py
@@ -0,0 +1,12 @@
+import random
+n=random.randint(1,100)
+while(True):
+    x=int(input("enter the number"))
+    if (x<n):
+        print("to low")
+    elif (x>n):
+        print("to high")
+    else :
+        print( "got the number")
+        break;
+            
