53d5f8bb08e465b30bb3fe9e826e2b372672e99e
Induhasini07 <hasinimittu6@gmail.com>
2025-04-17 11:34:15 +0530

    Create educative.py

---
+++ week4/edu.py
@@ -0,0 +1,150 @@
+# my_list = [1, 5, 10, 15, 20]
+# print(my_list)
+#  print([True, False, False, "1"])
+
+# my_list = list(["Lists", "are", "useful!"])
+# print(my_list)
+
+# my_list = [num for num in range (1,6)]
+# print(my_list)
+
+# my_list = [num for num in range (1,6)]
+# print(my_list)
+
+# element = my_list[2]
+# print(element)
+
+# my_list = [num for num in range (1,6)]
+# my_list[start:end]
+
+# my_list = [1, 5, 10, 15, 20, 25, 30, 35, 40]
+# my_new_list = [2:5]
+
+# print(my_new_list)
+
+# my_list = [num for num in range (1,6)]
+
+# last_element = my_list[-1]
+# print(last_element)
+
+# my_list = [num for num in range (1,6)]
+
+# try:
+#     element = my_list[1]
+#     print(element)
+# except IndexError:
+#     print("Index is out of range!")
+
+# my_list = [12, 19, 26, 23]
+# my_list.append(70)
+# print(my_list)
+
+# datatypes = ["python"]
+# datatypes.extend(["class", "students"])
+# print(datatypes)
+
+# bang_tan = ['Rm', 'Jin', 'min suga', 'Hobie', 'Jimin', 'Taehyunge','Jung kook']
+# bang_tan[2] = 'Suga'
+
+# print(bang_tan)
+# bang_tan = ['Kovla', 'RJ', 'shooky', 'mang', 'chimmy', 'Tata','Kookie']
+# bang_tan[3:5] = ['Mang', 'Chimmy']
+# print(bang_tan)  
+
+# bang_tan = ['Kovla', 'RJ', 'shooky', 'mang', 'chimmy', 'Tata','Kookie']
+# bang_tan[3:5] = ['Mang', 'Chimmy']
+# print(bang_tan)
+
+# blue_shades = ['navy', 'sky blue', 'sapphire', 'powder blue', 'teal', 'turquoise']
+# blue_shades[0:1] = ['denim', 'aqua']
+# print(blue_shades)
+
+
+# blue_shades = ['navy', 'sky blue', 'sapphire', 'powder blue', 'teal', 'turquoise']
+# blue_shades[0:3] = ['denim', 'aqua']
+# print(blue_shades)
+
+# blue_shades = ['navy', 'sky blue', 'sapphire', 'powder blue', 'teal', 'turquoise']
+
+# blue_shades = ['cobalt', 'azure', 'ice blue']
+# print(blue_shades)
+
+# blue_shades = ['navy', 'sky blue', 'sapphire', 'powder blue', 'teal', 'turquoise']
+
+#  blue_shades = ['cobalt', 'azure', 'ice blue']
+# print(blue_shades)  
+
+# space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+
+# del space_movies[1:3]
+# print(space_movies)
+
+# space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+# print(space_movies)
+
+# space_movies.remove('Gravity')
+# print(space_movies)
+
+# space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+# space_movies.clear()
+# print(space_movies)
+
+# space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+
+# removed_movie = space_movies.pop(2)
+# print("The removed movie is = " + removed_movie)
+# print(space_movies)
+
+# my_list = [1, 5, 10, 15, 20, 25, 30]
+# print(my_list[0:2])
+
+# my_list = [1, 5, 10, 15, 20, 25, 30]
+
+# my_list[0:2] = 50, 55
+# print(my_list)
+
+
+# my_list = [1, 5, 10, 15, 20, 25, 30]
+
+# my_list[0:2] = 50, 55
+# print(my_list)
+
+# my_list = [1, 5, 10, 15, 20, 25, 30]
+
+# slice_list_from_start = my_list[:4]
+# print(slice_list_from_start)
+
+# my_list = [1, 5, 10, 15, 20, 25, 30]
+
+# slice_list_till_end = my_list[4:]
+# print(slice_list_till_end)
+
+# my_list = [1, 5, 10, 15, 20, 25, 30]
+
+# last_three_elements = my_list[-3:]
+# print("Last three elements:", last_three_elements)
+
+# my_list = [1, 5, 10, 15, 20, 25, 30]
+
+# skip_every_second_from_end = my_list[-1:-6:-2]
+# print(skip_every_second_from_end)
+
+# my_list = [1, 5, 10, 15, 20, 25, 30]
+
+# reversed_list = my_list[::-1]
+# print(reversed_list)
+
+# numbers = [1, 2, 3, 4, 5]
+# squares = [num ** 2 for num in numbers]
+
+# print(squares)
+
+#  numbers = [1, 2, 3, 4, 5]
+# odd_numbers = [num for num in numbers if num % 2 != 0]
+
+# print(odd_numbers)
+
+# words = ["hoodie", "rivers", "cat", "monitor", "bag", "cup"]
+# short_words = [word for word in words if len(word) <= 5]
+
+# print(short_words)
